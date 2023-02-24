3ba30960112a1d041294e62d0ecc090e021b5346
Adam <62599990+Adam-M-04@users.noreply.github.com>
2023-02-24 22:23:49 +0100

    Mouse movement
    Added platform mouse movement

--- Assets/PlatformScript.cs
+++ Assets/PlatformScript.cs
@@ -1,37 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
+//using System.Numerics;
 using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
 {
-    public Rigidbody2D rigitBody;
-    public int speedMultiplier = 8;
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) == true)
-        {
-            rigitBody.velocity = UnityEngine.Vector2.left * speedMultiplier;
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow) == true)
-        {
-            rigitBody.velocity = UnityEngine.Vector2.right * speedMultiplier;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftArrow) == true)
-        {
-            rigitBody.velocity = UnityEngine.Vector2.zero;
-        }
-        if (Input.GetKeyUp(KeyCode.RightArrow) == true)
-        {
-            rigitBody.velocity = UnityEngine.Vector2.zero;
-        }
+        UnityEngine.Vector2 minScreenBounds = Camera.main.ScreenToWorldPoint(new UnityEngine.Vector2(0, 0));
+        UnityEngine.Vector2 maxScreenBounds = Camera.main.ScreenToWorldPoint(new UnityEngine.Vector2(Screen.width, Screen.height));
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new UnityEngine.Vector2(Mathf.Clamp(mousePosition.x, minScreenBounds.x + 1, maxScreenBounds.x - 1), transform.position.y);
+
     }
 }
